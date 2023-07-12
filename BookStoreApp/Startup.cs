92a663a1cbd345307d1b95fbbf45caaf6bfcfec9
Kolawole Abel <109530933+KolawoleAbel@users.noreply.github.com>
2023-07-12 14:51:01 +0100

    Update Startup.cs

--- BookStoreApp/Startup.cs
+++ BookStoreApp/Startup.cs
@@ -32,7 +32,7 @@ namespace BookStoreApp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Reliance Infosystems!");
                 });
             });
         }
